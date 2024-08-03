e7c7a8819f64e99198c2a4b844714de76d1df28d
tom p <txp001@hotmail.com>
2024-08-04 08:18:10 +1000

    read images for posts

--- Models/Post.cs
+++ Models/Post.cs
@@ -4,4 +4,5 @@ public class Post
 {
     public string? Message { get; set; }
     public Uri? PictureUrl { get; set; }
+    public required string Id { get; set; } 
 }
--- Services/Facebook.cs
+++ Services/Facebook.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using bodyline_sports.Models;
 using bodyline_sports.Options;
 using Microsoft.Extensions.Options;
@@ -15,6 +16,24 @@ public class Facebook : IFacebook
     private class GroupFeed
     {
         public required Post[] Data { get; set; }
+    }
+
+    private class Field
+    {
+        public required string Id { get; set;}
+        
+        [JsonPropertyName("object_id")]
+        public string? ObjectId { get; set;}
+    }
+
+    private class Picture
+    {
+        public required PictureData Data { get; set; }
+    }
+
+    private class PictureData
+    {
+        public required Uri Url { get; set; }
     }
 
     private readonly HttpClient _httpClient;
@@ -35,6 +54,22 @@ public class Facebook : IFacebook
     async Task<Post[]> IFacebook.GetGroupPosts(Group group)
     {
         var feed = await _httpClient.GetFromJsonAsync<GroupFeed>($"/{group.Id}/feed?access_token={_options.AccessToken}");
-        return feed?.Data ?? [];
+        var posts = feed?.Data ?? [];
+
+        foreach (var post in posts)
+        {
+            await SetPictureInPost(post);
+        }
+
+        return posts;
+    }
+
+    private async Task SetPictureInPost(Post post)
+    {
+        var pictureId = (await _httpClient.GetFromJsonAsync<Field>($"/{post.Id}?fields=object_id&access_token={_options.AccessToken}"))?.ObjectId;
+        if (pictureId is not null)
+        {
+            post.PictureUrl = (await _httpClient.GetFromJsonAsync<Picture>($"/{pictureId}/picture?redirect=false&access_token={_options.AccessToken}"))?.Data.Url;
+        }
     }
 }
