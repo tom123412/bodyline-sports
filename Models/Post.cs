f86513dfd4d4d469bef08b67be1051a2e603ae92
tom p <txp001@hotmail.com>
2024-08-10 09:03:20 +1000

    got posts showing

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace bodyline_sports.Models;
 
 public class Post
@@ -5,4 +7,7 @@ public class Post
     public string? Message { get; set; }
     public Uri? PictureUrl { get; set; }
     public required string Id { get; set; } 
+    
+    //[JsonPropertyName("updated_time")]
+    //public required DateTimeOffset UpdatedTime { get; set; }
 }
