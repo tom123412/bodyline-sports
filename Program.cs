c43839d78c60d0188c99d54bd3cb0b8e1a87475f
tom p <txp001@hotmail.com>
2024-08-21 15:57:17 +1000

    got login to facebook working on admin page

--- Program.cs
+++ Program.cs
@@ -3,10 +3,13 @@ using bodyline_sports.Components;
 using bodyline_sports.Http;
 using bodyline_sports.Options;
 using bodyline_sports.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Facebook;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Net.Http.Headers;
+using FacebookOptions = bodyline_sports.Options.FacebookOptions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,12 +21,25 @@ builder.Services.Configure<FacebookOptions>(builder.Configuration.GetSection(key
 builder.Services.Configure<ContactOptions>(builder.Configuration.GetSection(key: nameof(ContactOptions)));
 
 builder.Services
-    .AddAuthentication()
+    .AddAuthentication(options =>
+    {
+        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = FacebookDefaults.AuthenticationScheme;
+    })
     .AddFacebook(facebookOptions =>
     {
         facebookOptions.AppId = builder.Configuration["FacebookOptions:AppId"] ?? facebookOptions.AppId;
         facebookOptions.AppSecret = builder.Configuration["FacebookOptions:AppSecret"] ?? facebookOptions.AppSecret;
+        facebookOptions.Events = new()
+        {
+            OnRedirectToAuthorizationEndpoint = (context) =>
+            {
+                context.Response.Redirect($"{context.RedirectUri}&config_id={builder.Configuration["FacebookOptions:ConfigId"]}");
+                return Task.CompletedTask;
+            }
+        };
     })
+    .AddCookie()
     .AddIdentityCookies()
     ;
 
